eeddaf1ecfbff70633bbf993cbb4e6ced1d515e1
Makivay <Makivay@mail.ru>
2015-11-13 12:56:22 +0300

    dev

--- src/main/java/ru/Makivay/rest/OurAwesomeTableService.java
+++ src/main/java/ru/Makivay/rest/OurAwesomeTableService.java
@@ -1,8 +1,6 @@
 package ru.Makivay.rest;
 
 
-import com.atlassian.activeobjects.external.ActiveObjects;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -15,18 +13,16 @@ import javax.ws.rs.core.Response;
 @Path("/oat")
 @Produces({MediaType.TEXT_PLAIN, MediaType.TEXT_PLAIN})
 public class OurAwesomeTableService {
-    ActiveObjects activeObjects;
 
-    public OurAwesomeTableService(ActiveObjects activeObjects) {
-        this.activeObjects = activeObjects;
+    public OurAwesomeTableService() {
     }
 
     @GET
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     @Path("/get")
-    public Response getTable(){
+    public Response getTable() {
 
-        return  Response.ok("Hello rest!").build();
+        return Response.ok("Hello rest!").build();
     }
 
 
